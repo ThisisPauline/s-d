b733bddc9cf9183ab5ab88ec81f671413dea0022
Pauline Kraus <kraus.pauline1@gmail.com>
2022-12-08 10:31:45 +0100

    creating header

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,23 @@
+import logo from "../assets/logo.svg"
+import burger from "../assets/burger.svg"
+import * as React from 'react';
+import './header.css';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+    return (
+        <header className="header">
+        <div className = "loga-nav">
+          <img className = "logo" src={logo} alt="logo-Sür" />
+          <img className = "burger" src = {burger} alt = "" />
+        </div>
+        <h1 className="header-text">F*uck fashion</h1>
+      </header>
+    )
+};
+
+export default Header;
+
+
