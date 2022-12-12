ff384e35805d776066197713f995d60ac78a6c3d
Pauline Kraus <kraus.pauline1@gmail.com>
2022-12-12 19:52:48 +0100

    images added to DB

--- frontend/src/pages/ArticlesAdded.tsx
+++ frontend/src/pages/ArticlesAdded.tsx
@@ -1,42 +1,43 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./articles.css"
+import "./articles.css";
+import { Link } from "react-router-dom";
 
-
-interface Articles{
-    name: string;
-    price: number;
-    category : string;
-    id: number;
-  }
+interface Articles {
+  name: string;
+  price: number;
+  category: string;
+  id: number;
+  images: string;
+}
 
 const ArticlesAdded = () => {
-    const [articles, setArticles] = useState<Articles[]>([]);
-    
-    useEffect(() => {
-        axios
-          .get<Articles[]>("http://localhost:5005/articles")
-          .then((response) => response.data)
-          .then((data) => setArticles(data));
-      }, []);
-    
-     
-      console.log(articles);
-      if (articles === undefined) return <p>Loading...</p>;
+  const [articles, setArticles] = useState<Articles[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Articles[]>("http://localhost:5005/articles")
+      .then((response) => response.data)
+      .then((data) => setArticles(data));
+  }, []);
+
+  console.log(articles);
+  if (articles === undefined) return <p>Loading...</p>;
   return (
     <div>
-        {articles.length != 0?
- <ul>
- {articles.map((article) => {
- console.log(article);
- return (
- <li>
- {article.name}
- </li>
- );
- })}
- </ul>
-    : <p>Loading...</p>}</div>
-)};
+      {articles.length != 0 ? (
+        articles.map((article) => (
+          <div className="individual-product">
+            <img className="image-product" src={article.images} />
+            <p className="product-title">{article.name}</p>
+            <p className="product-price">{article.price}€</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default ArticlesAdded;
