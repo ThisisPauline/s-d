7622ea4caec31997a2600bebc152241f75ba69b3
Pauline Kraus <kraus.pauline1@gmail.com>
2022-12-12 13:41:46 +0100

    Backend, Express, link to DB

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+const database = require("./database");
+
+const BACKEND_PORT = process.env.BACKEND_PORT ?? 5005;
+
+app.get("/", (req, res) => {
+  let output = "";
+  let error = false;
+
+  database
+    .getConnection()
+    .then(() => {
+      output += "Database connection working well.";
+    })
+    .catch((err) => {
+      error = true;
+      console.error(err);
+      output += "Database connection malfunctioning.";
+    })
+    .finally(() => {
+      if (error) {
+        res.status(500).send(output);
+      } else {
+        res.status(200).send("Welcome to our API!" + output);
+      }
+    });
+});
+
+app.listen(BACKEND_PORT, () => {
+  console.log("Listening on port", BACKEND_PORT);
+});
